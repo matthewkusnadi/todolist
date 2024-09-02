96872909d5992bbcdc2410851d286b1a61ba025a
Matthew Kusnadi <matthew.kusnadi@hotmail.com>
2024-09-02 13:10:13 +1000

    first commit

---
+++ ToDoList.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+ 
