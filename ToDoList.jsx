d88a28823842ef060e1c4b31c90ffeafd49ee1a6
Matthew Kusnadi <matthew.kusnadi@hotmail.com>
2024-09-02 13:12:19 +1000

    edit

--- ToDoList.jsx
+++ ToDoList.jsx
@@ -17,4 +17,58 @@ function ToDoList() {
 
   function deleteTask(index) {
     const updatedTasks = tasks.filter((_, i) => i !== index);
- 
+    setTasks(updatedTasks);
+  }
+
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
+  }
+
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  }
+
+  return (
+    <div className="to-do-list">
+      <h1>To Do List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+            <button className="move-button" onClick={() => moveTaskUp(index)}>☝️</button>
+            <button className="move-button" onClick={() => moveTaskDown(index)}>👇</button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default ToDoList;
